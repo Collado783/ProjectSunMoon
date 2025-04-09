dc5f87c581644c45c844112a7a31593e04f3cb9e
Collado783 <mcollador@edu.tecnocampus.cat>
2025-04-09 18:03:15 +0200

    correction proyectil again

--- Assets/ProjectileBehavior.cs
+++ Assets/ProjectileBehavior.cs
@@ -5,7 +5,7 @@ public class ProjectileBehavior : MonoBehaviour
     public float Speed = 4.5f;
     private void Update()
     {
-        transform.position += transform.right * Time.deltaTime * Speed;
+        transform.position -= transform.right * Time.deltaTime * Speed;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
