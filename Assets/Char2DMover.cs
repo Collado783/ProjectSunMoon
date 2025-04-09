c7750886a088246b9b2b7b4e20f585de0890534d
Collado783 <mcollador@edu.tecnocampus.cat>
2025-04-09 17:35:50 +0200

    Base

---
+++ Assets/Char2DMover.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Char2DMover : MonoBehaviour
+{
+    public float MovementSpeed = 1;
+    public float JumpForce = 1;
+    private Rigidbody2D _rigidbody;
+    public ProjectileBehavior ProjectilePrefab;
+    public Transform LaunchOffset;
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.linearVelocity.y)<0.001f)
+        {
+            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(ProjectilePrefab, LaunchOffset.position, transform.rotation);
+        }
+    }
+
+}
---
+++ Assets/ProjectileBehavior.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ProjectileBehavior : MonoBehaviour
+{
+    public float Speed = 4.5f;
+    private void Update()
+    {
+       transform.position = transform.right *Time.deltaTime*Speed;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
