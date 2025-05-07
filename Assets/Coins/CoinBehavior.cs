93e20000b899fa4dcb9f50fd5de5f2a83e263876
Collado783 <mcollador@edu.tecnocampus.cat>
2025-05-07 23:02:49 +0200

    ertyuk

--- Assets/Coins/CoinBehavior.cs
+++ Assets/Coins/CoinBehavior.cs
@@ -5,9 +5,15 @@ public class CoinBehavior : MonoBehaviour
 {
     public int coinCount;
     public Text coinText;
+    public GameObject goalCoin;
+    public int maxCoins = 12;
 
     private void Update()
     {
         coinText.text = coinCount.ToString();
+
+        if (coinCount == maxCoins) {}
+
+
     }
 }
