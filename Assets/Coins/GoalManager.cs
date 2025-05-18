35bbab8b5cceaa6330e18124400d8781429cbac3
Collado783 <mcollador@edu.tecnocampus.cat>
2025-05-18 19:01:42 +0200

    TileMap

--- Assets/Coins/GoalManager.cs
+++ Assets/Coins/GoalManager.cs
@@ -18,7 +18,7 @@ public class GoalManager : MonoBehaviour
             return;
         }
         goalManager = this;
-        DontDestroyOnLoad(gameObject);
+        //DontDestroyOnLoad(gameObject);
     }
 
     private void Start()
